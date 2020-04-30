32bd1971fb1e59bb27e9e126c31e1ad918a45590
NoahGiesel <noahgiesel@gmail.com>
2020-04-30 13:47:56 +0200

    added filter to img and txt. fix about

--- src/Components/ParallaxSection/PrallaxSection.js
+++ src/Components/ParallaxSection/PrallaxSection.js
@@ -7,6 +7,10 @@ function ParallaxSection() {
     <div className="ParallaxSection container">  
         
         <div className="parallax">
+          <div className="text">
+            <h2>Work</h2>
+            <h3>WITH US </h3>
+          </div>
          </div>
     </div>
   );

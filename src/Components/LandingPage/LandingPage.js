690463b6ea826faa09deaa3197ac0781401b41f3
NoahGiesel <noahgiesel@gmail.com>
2020-05-01 13:10:52 +0200

    dixed footer and landingpage- installed react-icons

--- src/Components/LandingPage/LandingPage.js
+++ src/Components/LandingPage/LandingPage.js
@@ -1,6 +1,7 @@
 import React   from 'react';
 import './LandingPage.css';
- 
+import { FaTwitter, FaLinkedinIn, FaInstagram, FaYoutube } from 'react-icons/fa';
+
 import black_cone from '../../img/black_cone.png'  
 
 function LandingPage() { 
@@ -11,9 +12,18 @@ function LandingPage() {
        <a  href="##"><img className="black_cone" src={black_cone} alt={"black_cone"}/> </a>  
         </div>
         <div className="text-section">
-          <h3> Lorem ciao come va io sto bnene </h3>
-          <p>Lorem40 adjkasoid erfpkk rpfr erpofkg rpeofkop reeropkf oprefk op</p> 
-          <p>Lorem40 adjkasoid erfpkk rpfr erpofkg rpeofkop reeropkf oprefk op</p> 
+          <h3> ICE CREAM CAFE </h3>
+          <div className="paragraf">
+            <p>One of its kind! Dicover more than 20 different flavors  one different from another </p> 
+            <p>Check out our current open positions to be part of this amazing group of talented people!</p>
+             
+            <div className="socials">
+              <FaTwitter className="icon twitter" />
+              <FaLinkedinIn className="icon Linkedin"/>
+              <FaInstagram className="icon instagram"/>
+              <FaYoutube className="icon  youtube"/>
+              </div>
+          </div>
         </div>
     </div>
   );

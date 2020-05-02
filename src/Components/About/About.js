44accb277a8d533b799462752258a4e116d3ef7a
NoahGiesel <noahgiesel@gmail.com>
2020-05-02 15:50:06 +0200

    fixed mobile navigation and about UI bug

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -10,7 +10,7 @@ function About( props ) {
         <div className="container">
             <div className="left-section">
                 <div className="wrapper">
-                    <h2 className="first-h2">{props.Title1}</h2>
+                    <h2 className="first-h2"> {props.Title1}</h2>
                     <h2 className="second-h2">{props.Title2} </h2>
                     <p>  {props.p} </p>
                 </div>
--- src/Components/LandingPage/LandingPage.js
+++ src/Components/LandingPage/LandingPage.js
@@ -4,24 +4,24 @@ import { FaTwitter, FaLinkedinIn, FaInstagram, FaYoutube } from 'react-icons/fa'
 
 import black_cone from '../../img/black_cone.png'  
 
-function LandingPage() { 
+function LandingPage()  { 
 
   return (
     <div className="LandingPage">  
         <div className="ice-section">
        <a  href="##"><img className="black_cone" src={black_cone} alt={"black_cone"}/> </a>  
         </div>
-        <div className="text-section">
-          <h3> ICE CREAM CAFE </h3>
+        <div className="text-section" >
+          <h3> ICE CREAM CAFE  </h3>
           <div className="paragraf">
             <p>One of its kind! Dicover more than 20 different flavors  one different from another </p> 
             <p>Check out our current open positions to be part of this amazing group of talented people!</p>
              
             <div className="socials">
-              <FaTwitter className="icon twitter" />
-              <FaLinkedinIn className="icon Linkedin"/>
-              <FaInstagram className="icon instagram"/>
-              <FaYoutube className="icon  youtube"/>
+                <FaTwitter className="icon twitter" />
+                <FaLinkedinIn className="icon Linkedin"/>
+                <FaInstagram className="icon instagram"/>
+                <FaYoutube className="icon  youtube"/>
               </div>
           </div>
         </div>
